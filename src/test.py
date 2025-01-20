fc502aa7e3cebe6c28a1c9da238fa377998fb66d
furkacz <100477457+furkacz@users.noreply.github.com>
2025-01-20 20:21:55 +0100

    Fixed NDCG and added F1

--- src/test.py
+++ src/test.py
@@ -1,11 +1,9 @@
 from src import pyenv
 from src.db import connector
 
-from sklearn.metrics import ndcg_score
-
 from statistics import fmean
 from pprint import pprint
-
+from math import log2
 
 # make queries to the system
 def query_predictions(query):
@@ -28,20 +26,36 @@ def MRR(truth, predictions):
     return float(fmean(rr))
 
 
-#NDCG
+# NDCG
 def apply_relevance(mask, predictions):
     relevant = [mask.get(pred) for pred in predictions]
-    return [0 if rel is None else rel for rel in relevant]
+    return [0 if rel is None else rel for rel in relevant][:len(mask)]
 
 
-def NDCG(truth, predictions):
-    relevance = [3, 2, 2, 1, 1]
+def DCG(relevances):
+    return float(sum(list(map(lambda rel: rel[1] / log2(rel[0] + 2), enumerate(relevances)))))
+
+
+def NDCG(truth, predictions, relevance=[3, 2, 2, 1, 1]):
     relevant_masks = [{t: relevance[i] for (i, t) in enumerate(t_vec)} for t_vec in truth]
 
-    y_true = [relevance for _ in truth]
+    y_true = [relevance[:len(t)] for t in truth]
     y_pred = [apply_relevance(pair[0], pair[1]) for pair in list(zip(relevant_masks, predictions))]
 
-    return float(ndcg_score(y_true, y_pred))
+    rows = list(zip(y_true, y_pred))
+
+    return float(fmean([DCG(row[1]) / DCG(row[0]) for row in rows]))
+
+
+# F1
+def occurences(true, pred):
+    return int(sum([pred.count(t) for t in true]))
+
+
+def F1(truth, predictions):
+    rows = list(zip(truth, predictions))
+    hits = list(map(lambda row: occurences(row[0], row[1]) / len(row[0]), rows))
+    return float(fmean(hits))
 
 
 if __name__ == "__main__":
@@ -54,10 +68,21 @@ if __name__ == "__main__":
 
     p_segments, p_articles = list(zip(*list(map(lambda query: query_predictions(query), queries))))
 
+    print('''
+    MRR - mean reciprocal rank
+    NDCG - normalized discounted gain (rel = [3, 2, 2, 1, 1])
+    NDCG2 - binary normalized discounted gain (rel = [1, 1, 1, 1, 1])
+    F1 - in this case its average hit rate
+    ''')
+
     results = {
-        'MRR_segments': MRR(t_segments, p_segments),
+        'F1_articles': F1(t_articles, p_articles),
+        'F1_segments': F1(t_segments, p_segments),
         'MRR_articles': MRR(t_articles, p_articles),
-        'NDCG_segments': NDCG(t_segments, p_segments),
-        'NDCG_articles': NDCG(t_articles, p_articles)
+        'MRR_segments': MRR(t_segments, p_segments),
+        'NDCG2_articles': NDCG(t_articles, p_articles, [1, 1, 1, 1, 1]),
+        'NDCG2_segments': NDCG(t_segments, p_segments, [1, 1, 1, 1, 1]),
+        'NDCG_articles': NDCG(t_articles, p_articles),
+        'NDCG_segments': NDCG(t_segments, p_segments)
     }
     pprint(results)
